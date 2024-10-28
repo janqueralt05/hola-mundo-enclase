1ce150c79c6e1748043740234d7100516cd374b5
Jan Queralt <jan@alumnes.ilerna.com>
2024-10-28 19:22:46 +0100

    change test.java

--- test-java-project/src/test/test.java
+++ test-java-project/src/test/test.java
@@ -1,10 +1,11 @@
+package test;
 
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo 🌏🌎🌍🌍");
 		
 	}
 
